7479793dc8b7bbd343e8ad87cef02b6dd6c17953
ParasailNumerous <305966967+ParasailNumerous@users.noreply.github.com>
2026-07-31 17:35:02 +0000

    Minfy and add cache busters to CSS files

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,26 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { transform } from 'lightningcss';
 
 export default function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy({ "public": "/" });
+  // Inspired by https://codeberg.org/amberstarlight/website/src/commit/21ba38eac36ec72b8710aca371239a42e3163442/eleventy.config.js#L57
+  // (thanks!!!)
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "css",
+    transforms: [
+      async function (content) {
+        const { page } = this;
+        const { code } = transform({
+          filename: page.inputPath,
+          code: Buffer.from(content),
+          minify: true
+        });
+
+        return code;
+      }
+    ]
+  });
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["webp", "png", "jpeg"]
   });
