092a7c45ebc69266f147ebad4a85a429da3ef1dc
ParasailNumerous <305966967+ParasailNumerous@users.noreply.github.com>
2026-07-31 05:40:30 +0000

    Push new pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy({ "public": "/" });
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
   return {
     dir: {
